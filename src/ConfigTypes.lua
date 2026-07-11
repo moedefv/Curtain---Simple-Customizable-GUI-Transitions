746a0371e0e5d38e416f6ff8c0ff364e4d501a92
moedefv <laythaldu0@gmail.com>
2026-07-10 23:05:50 -0400

    Create ConfigTypes.lua

---
+++ src/ConfigTypes.lua
@@ -0,0 +1,27 @@
+export type GridConfig = {
+	Speed: number?,
+	Color: Color3?,
+	EasingStyle: Enum.EasingStyle?,
+}
+
+export type SlideConfig = {
+	Speed: number?,
+	Color: Color3?,
+	Direction: string?,
+	EasingStyle: Enum.EasingStyle?,
+	ReverseOut: boolean?,
+}
+
+export type FadeConfig = {
+	Speed: number?,
+	Color: Color3?,
+	EasingStyle: Enum.EasingStyle?,
+}
+
+export type IrisConfig = {
+	Speed: number?,
+	Color: Color3?,
+	EasingStyle: Enum.EasingStyle?,
+}
+
+return {}
